cecc324673600d303ded4ddffa044dc4c4a06297
Ashehal Sadik <ashehalomarsadik@gmail.com>
2026-05-13 15:40:26 +0500

    Fix enumeration order stat, i -> i, stat in morphological_engine

--- morphological_engine.py
+++ morphological_engine.py
@@ -2,20 +2,20 @@ import cv2
 import numpy as np
 
 def open(img, kernel):
-    return cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+    return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
 def close(img, kernel):
     return cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 
 def size_filter(img, stats, pRange):
     valid_people = []
-    for stat, i in enumerate(stats):
-        x, y, w, h, area = stat
+    for i, stat in enumerate(stats):
+        x, y, w, h, area, centroid = stat
         if pRange[0] <= area <= pRange[1]:
             valid_people.append({
                 'bbox': [x, y, w, h],
                 'area': area,
-                'center': [x + w / 2, y + h / 2]
+                'center': centroid
             })
         else:
             print(f"Discarding blob {i}: area = {area} is outside the image range {pRange[0]}-{pRange[1]}")
